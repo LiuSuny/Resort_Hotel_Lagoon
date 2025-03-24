f0a417c74ff9cab1770757c1e7a1db379cc25deb
Sunday A <119320935+LiuSuny@users.noreply.github.com>
2025-03-24 12:27:40 +0300

    Customize validation done with ability to create resort

---
+++ ResortLagoon.Infrastructure/Config/PriceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ResortLagoon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResortLagoon.Infrastructure.Config
+{
+    public class PriceConfiguration : IEntityTypeConfiguration<Villa>
+    {
+        public void Configure(EntityTypeBuilder<Villa> builder)
+        {
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ ResortLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResortLagoon.Domain.Entities;
+using ResortLagoon.Infrastructure.Data;
+
+namespace ResortLagoon.Web.Controllers
+{
+    public class VillaController(ApplicationDbContext _db) : Controller
+    {
+        public IActionResult Index()
+        {
+            var villa = _db.Villas.ToList();
+            return View(villa);
+        }
+
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(villa);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
