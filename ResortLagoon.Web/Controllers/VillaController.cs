02db8f3a4f7af9cf1406f8b40d5b73e6de7d331d
Sunday A <119320935+LiuSuny@users.noreply.github.com>
2025-04-06 09:06:54 +0300

    Crud on Images Done

--- ResortLagoon.Web/Controllers/VillaController.cs
+++ ResortLagoon.Web/Controllers/VillaController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using ResortLagoon.Application.Common.Interfaces;
 using ResortLagoon.Domain.Entities;
 using ResortLagoon.Infrastructure.Data;
 
 namespace ResortLagoon.Web.Controllers
 {
-    public class VillaController(IUnitOfWork _unitOfWork) : Controller
+    public class VillaController(IUnitOfWork _unitOfWork, IWebHostEnvironment _webHost) : Controller
     {
         public IActionResult Index()
         {
@@ -27,7 +28,24 @@ namespace ResortLagoon.Web.Controllers
             }
             if (ModelState.IsValid)
             {
-                _unitOfWork.Villa.Add(villa);
+                if(villa.Image != null)
+                {
+                    //retrive with random guid with upload image
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(villa.Image.FileName);
+                    // path root
+                    string imagePath = Path.Combine(_webHost.WebRootPath, @"images\VillaImage");
+
+                    //create and joing the images and copy it to villa folder in www.root fold
+                    using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+                    villa.Image.CopyTo(fileStream);
+
+                    villa.ImageUrl = @"\images\VillaImage\" + fileName;
+                }
+                else
+                {
+                    villa.ImageUrl = "https://placehold.co/600x400";
+                }
+                    _unitOfWork.Villa.Add(villa);
                 _unitOfWork.Save();
                 TempData["success"] = "The villa has been created successfully.";
                 return RedirectToAction(nameof(Index));
@@ -52,6 +70,29 @@ namespace ResortLagoon.Web.Controllers
         {
             if (ModelState.IsValid && obj.Id > 0)
             {
+                if (obj.Image != null)
+                {
+                    //retrive with random guid with upload image
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+                    // path root
+                    string imagePath = Path.Combine(_webHost.WebRootPath, @"images\VillaImage");
+
+                    if (!string.IsNullOrEmpty(obj.ImageUrl)) //if there is existing image and need changes
+                    {
+                        var oldImagePath = Path.Combine(_webHost.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+                    //create and joing the images and copy it to villa folder in www.root fold
+                    using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+                    obj.Image.CopyTo(fileStream);
+
+                    obj.ImageUrl = @"\images\VillaImage\" + fileName;
+                }
+               
                 _unitOfWork.Villa.Update(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "The villa has been updated successfully.";
@@ -76,6 +117,16 @@ namespace ResortLagoon.Web.Controllers
             Villa? objFromDb = _unitOfWork.Villa.Get(u => u.Id == obj.Id);
             if (objFromDb is not null)
             {
+
+                if (!string.IsNullOrEmpty(objFromDb.ImageUrl)) //if there is existing image and need changes
+                {
+                    var oldImagePath = Path.Combine(_webHost.WebRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
                 _unitOfWork.Villa.Remove(objFromDb);
                 _unitOfWork.Save();
                 TempData["success"] = "The villa has been deleted successfully.";
