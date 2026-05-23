df124cfc419e8bfea1458090024a112f0c2e90a1
Sunday A <119320935+LiuSuny@users.noreply.github.com>
2026-05-23 16:02:54 +0200

    UPDATED DATABASE

--- ResortLagoon.Infrastructure/Migrations/20260523135352_AddTableDB.cs
+++ ResortLagoon.Infrastructure/Migrations/20260523135352_AddTableDB.cs
@@ -3,10 +3,12 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace ResortLagoon.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class IdentityUserTable : Migration
+    public partial class AddTableDB : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -30,6 +32,8 @@ namespace ResortLagoon.Infrastructure.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
@@ -48,6 +52,26 @@ namespace ResortLagoon.Infrastructure.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Villas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Sqft = table.Column<double>(type: "float", nullable: false),
+                    Occupancy = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Villas", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -156,6 +180,95 @@ namespace ResortLagoon.Infrastructure.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "Amenities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VillaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amenities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Amenities_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "villaNumbers",
+                columns: table => new
+                {
+                    Villa_Number = table.Column<int>(type: "int", nullable: false),
+                    VillaId = table.Column<int>(type: "int", nullable: false),
+                    SpecialDetails = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_villaNumbers", x => x.Villa_Number);
+                    table.ForeignKey(
+                        name: "FK_villaNumbers_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdateDate" },
+                values: new object[,]
+                {
+                    { 1, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", "Royal Villa", 4, 200m, 550.0, null },
+                    { 2, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x401", "Premium Pool Villa", 4, 300m, 550.0, null },
+                    { 3, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x402", "Luxury Pool Villa", 4, 400m, 750.0, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "Id", "Description", "Name", "VillaId" },
+                values: new object[,]
+                {
+                    { 1, null, "Private Pool", 1 },
+                    { 2, null, "Microwave", 1 },
+                    { 3, null, "Private Balcony", 1 },
+                    { 4, null, "1 king bed and 1 sofa bed", 1 },
+                    { 5, null, "Private Plunge Pool", 2 },
+                    { 6, null, "Microwave and Mini Refrigerator", 2 },
+                    { 7, null, "Private Balcony", 2 },
+                    { 8, null, "king bed or 2 double beds", 2 },
+                    { 9, null, "Private Pool", 3 },
+                    { 10, null, "Jacuzzi", 3 },
+                    { 11, null, "Private Balcony", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "villaNumbers",
+                columns: new[] { "Villa_Number", "SpecialDetails", "VillaId" },
+                values: new object[,]
+                {
+                    { 101, null, 1 },
+                    { 102, null, 1 },
+                    { 103, null, 1 },
+                    { 104, null, 1 },
+                    { 201, null, 2 },
+                    { 202, null, 2 },
+                    { 203, null, 2 },
+                    { 301, null, 3 },
+                    { 302, null, 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Amenities_VillaId",
+                table: "Amenities",
+                column: "VillaId");
+
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
                 table: "AspNetRoleClaims",
@@ -194,11 +307,19 @@ namespace ResortLagoon.Infrastructure.Migrations
                 column: "NormalizedUserName",
                 unique: true,
                 filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_villaNumbers_VillaId",
+                table: "villaNumbers",
+                column: "VillaId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Amenities");
+
             migrationBuilder.DropTable(
                 name: "AspNetRoleClaims");
 
@@ -215,10 +336,16 @@ namespace ResortLagoon.Infrastructure.Migrations
                 name: "AspNetUserTokens");
 
             migrationBuilder.DropTable(
+                name: "villaNumbers");
+
+            migrationBuilder.DropTable(
                 name: "AspNetRoles");
 
             migrationBuilder.DropTable(
                 name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Villas");
         }
     }
 }
---
+++ ResortLagoon.Web/Controllers/PlayingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResortLagoon.Web.Controllers
+{
+    public class PlayingController : Controller
+    {
+
+        public IActionResult Edit(string id)
+        {
+            return View(id);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
